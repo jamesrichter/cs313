d9c69466250b0e7a4298f04e52b8583e7a4b1e60
jamesrichter <jamesaugustrichter@gmail.com>
2015-02-12 14:33:48 -0700

    lk

--- insertPicture.php
+++ insertPicture.php
@@ -16,18 +16,39 @@
 $title = $_POST['txtTitle'];
 $image = $_POST['txtImage'];
 
-// we could put additional checks here to verify that all this data is actually provided
-
+function loadDatabase()
+{
 	$dbHost = "http://php-jamesrichter.rhcloud.com";
 	$dbPort = "3306";
 	$dbUser = "adminYwPVfAG";
-	$dbPass = "pCTEtPQQJZI8";
+	$dbPassword = "pCTEtPQQJZI8";
 	$dbName = "picSite";
+	$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	if ($openShiftVar === null || $openShiftVar == "")
+	{
+// Not in the openshift environment
+//echo "Using local credentials: ";
+//require("setLocalDatabaseCredentials.php");
+	}
+	else
+	{
+// In the openshift environment
+//echo "Using openshift credentials: ";
+		$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+		$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+		$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	}
+//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+	$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+	return $db;
+}
+
 
 try
 {
 	// Create the PDO connection
-	$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+	$db = loadDatabase();
 
 	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
 	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
