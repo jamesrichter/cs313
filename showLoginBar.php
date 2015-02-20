4ba07576d2f93e01d6dbf4ac5e2884b9e6453a8d
jamesrichter <jamesaugustrichter@gmail.com>
2015-02-20 15:38:16 -0700

    lk

--- showLoginBar.php
+++ showLoginBar.php
@@ -4,7 +4,8 @@ function showLoginBar(){
 	echo "
 	<div id='loginBar'>
 		You are logged in as " . $_SESSION["user"] . ". Not you? 
-		<a href='login.php'>Login</a>
+		<a href='login.php'>Login</a>  
+		<a href='signUp.php>Sign Up</a>'
 	</div>
 
 	";
