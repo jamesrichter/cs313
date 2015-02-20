ff8fae6a6a0e812ee0c0338122a3e198f592455d
jamesrichter <jamesaugustrichter@gmail.com>
2015-02-20 13:45:23 -0700

    lk

--- login.php
+++ login.php
@@ -1,40 +1,85 @@
 <?php
+require("password.php"); // used for password hashing.
+require("loadPicDatabase.php");
 session_start();
 
-include "loadPicDatabase.php";
-/*
-if !isset($SESSION["user"]){
-	$_SESSION["user"] = "adminYwPVfAG";
-	$_SESSION["password"] = "pCTEtPQQJZI8";
-}
-*/
-if(isset($_POST['submit'])){ 
-	$_SESSION["user"] = $_POST['username'];
-	$_SESSION["password"] = $_POST['password'];
+$badLogin = false;
 
-	try {
-		loadDatabase($_SESSION["user"], $_SESSION["password"]);
-		$sql = 'SELECT * FROM picture';
-		header("Location: https://php-jamesrichter.rhcloud.com/showPictures.php");
+if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
+{
+	// they have submitted a username and password for us to check
+	$username = $_POST['txtUsername'];
+	$password = $_POST['txtPassword'];
+
+	try
+	{
+		// Create the PDO connection
+		$db = loadDatabase("adminYwPVfAG","pCTEtPQQJZI8");
+
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+		$query = 'SELECT password FROM login WHERE username=:username';
+
+		$statement = $db->prepare($query);
+		$statement->bindParam(':username', $username);
+
+		$result = $statement->execute();
+
+		if ($result)
+		{
+			$row = $statement->fetch();
+			$hashedPasswordFromDB = $row['password'];
+
+			// now check to see if the hashed password matches
+			if (password_verify($password, $hashedPasswordFromDB))
+			{
+				// password was correct, put the user on the session, and redirect to home
+				$_SESSION['username'] = $username;
+				header("Location: showPictures.php");
+				die(); // we always include a die after redirects.
+			}
+			else
+			{
+				$badLogin = true;
+			}
+
+		}
+		else
+		{
+			$badLogin = true;
+		}
+	}
+	catch (Exception $ex)
+	{
+		// Please be aware that you don't want to output the Exception message in
+		// a production environment
+		echo "Error with DB. Details: $ex";
 		die();
 	}
-	catch (Exception $ex){
-		echo 'Could not login.';
-		echo '
-			<form  method="post" action="login.php"  id="searchform"> 
-			  Username<input  type="text" name="username"><br/>
-			  Password<input  type="password" name="password"> 
-			  <input  type="submit" name="submit" value="Login"> 
-			</form>
-			';
-	}
+
 }
 
-echo '
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log In</title>
+</head>
+
+<body>
+<div>
+
+<?php
+if ($badLogin)
+{
+	echo "Incorrect username or password!<br/><br/>Please try again:<br/>\n";
+}
+?>
+
 <form  method="post" action="verifyLogin.php"  id="searchform"> 
-  Username<input  type="text" name="username"><br/>
-  Password<input  type="password" name="password"> 
+  Username<input  type="text" name="txtUsername"><br/>
+  Password<input  type="password" name="txtPassword"> 
   <input  type="submit" name="submit" value="Login"> 
 </form>
-';
-?>
