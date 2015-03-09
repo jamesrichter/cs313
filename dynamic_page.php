4925e6d0a758a1f0ab3bf8f5f336e0bfec039c90
jamesrichter <jamesaugustrichter@gmail.com>
2015-03-09 01:00:18 -0600

    lj

--- dynamic_page.php
+++ dynamic_page.php
@@ -2,6 +2,7 @@
 include 'loadPicDatabase.php';
 include 'showLoginBar.php';
 session_start();
+showLoginBar();
 
 if (isset($_GET['id'])) 
 {
@@ -11,72 +12,81 @@ else {
 	echo "Image not found.";
 }
 
-function getPicSite() {
+function getPicSite($picID) {
 	$conn = loadDatabase();
 
 	try {
-		$sql = 'SELECT * FROM picture where pictureID = $picID';  
+		$sql = 'SELECT * FROM picture WHERE pictureID=:id';
 		$stmt = $conn->prepare($sql);
 		$stmt->bindParam(":id", $picID);
-		echo $stmt;
 		$stmt->execute();
 		$data = $stmt->fetchAll();
 		$stmt->closeCursor();
-		echo $sql;
 	} catch (PDOException $ex) {
 		echo 'PDO error in model.';
 	}
 
 	if (is_array($data)) {
-		echo "fine";
 		return $data;
 	} else {
 		return FALSE;
 	}
 }
 
-$test = getPicSite();
-if (isset($test))
-{
-	echo "set";
-}
-if ($test == FALSE)
-{
-	echo "false";
-}
-if ($test == TRUE)
-{
-	echo "true";
-}
-if (is_array($test))
-{
-	echo "array";
-}
-if ($test != FALSE)
-{
-	echo "not galse";
+function getComments($picID){
+	$conn = loadDatabase();
+	try {
+		$sql = 'SELECT * FROM picture 
+				INNER JOIN comment ON comment.pictureID=picture.pictureID
+				INNER JOIN login ON comment.userID=login.id
+				WHERE picture.pictureID=1';  
+		$stmt = $conn->prepare($sql);
+		$stmt->bindParam(":id", $picID);
+		$stmt->execute();
+		$data = $stmt->fetchAll();
+		$stmt->closeCursor();
+	}
+	catch (PDOException $ex) {
+		echo 'PDO error in model';
+	}
+	if (is_array($data)) {
+		return $data;
+	} else {
+		return FALSE;
+	}
 }
 
-echo $test;
-foreach ($test as $key => $value) {
-	echo $key;
-	echo $value;
-	/*echo " " .
+$picture = getPicSite($picID);
+$comments = getComments($picID);
+
+foreach ($picture as $key => $value) {
+	echo " <h1>" .
 	$value['title'] . 
-	" <img src='" .
+	"</h1><br/><img src='" .
 	$value['image'] .
-	"' width='600'>";*/
+	"' width='600'><br/>";
 }
-/**
-showLoginBar();
 
-echo $data['pictureID'];
-echo $data['title'];
-echo $data['image'];
-echo $data['userID'];
-echo "
-</body>
-</html>
-";
-**/
+foreach ($comments as $key => $value) {
+	echo "<br/>" .
+	$value['username'] .
+	"<br/>" .
+	$value['text'] .
+	"<br/>";
+}
+
+if (isset($_SESSION['userID']) && ($_SESSION['username'] != 'guest'))
+{
+	$_SESSION['picID'] = $picID;
+	echo '<br/>Add a Comment:
+
+	<form id="commentForm" action="insertComment.php" method="POST">
+
+	<textarea id="txtText" name="txtText"></textarea>
+	<br />
+
+	<input type="submit" value="Add Comment" />
+
+</form>';
+}
 ?>
