7a5835297a114bd0ff62946737973c16923466e1
jamesrichter <jamesaugustrichter@gmail.com>
2015-03-09 01:09:47 -0600

    lj

--- createAccount.php
+++ createAccount.php
@@ -1,6 +1,6 @@
 <?php
 /**********************************************************
-* File: singup.php
+* File: createAccount.php
 * Author: Br. Burton
 * 
 * Description: Allows a user to enter a new username
@@ -10,9 +10,10 @@
 *   which does the actual creation.
 *
 ***********************************************************/
-
+session_start();
 // used for password hashing. If we don't have php 5.5 or later (e.g. openshift)
 // then we will need to have this file in our path (e.g., in the current directory)
+require("loadPicDatabase.php");
 require("password.php");
 
 // get the data from the POST
@@ -27,23 +28,16 @@ if (!isset($username) || $username == ""
 }
 
 // Let's not allow HTML in our usernames. It would be best to detect this before
-// submitting the form and preven the submission.
+// submitting the form and prevent the submission.
 $username = htmlspecialchars($username);
 
 // Get the hashed password.
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-
-// It would be better to store these in a different file
-$dbUser = 'ta6user';
-$dbPass = 'ta6pass';
-$dbName = 'LoginTest';
-$dbHost = '127.0.0.1'; // for my configuration, I need this rather than 'localhost'
-
 try
 {
 	// Create the PDO connection
-	$db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+	$db = loadDatabase();
 
 	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
 	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -71,7 +65,7 @@ catch (Exception $ex)
 
 
 // finally, redirect them to the sign in page
-header("Location: signIn.php");
+header("Location: logIn.php");
 die(); // we always include a die after redirects. In this case, there would be no
        // harm if the user got the rest of the page, because there is nothing else
        // but in general, there could be things after here that we don't want them
